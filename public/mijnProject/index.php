a19eea67a09b06060fa468970133964f8ea7a122
GodlyFoxy <godlyfoxxy@gmail.com>
2021-06-01 15:10:25 +0000

    fix

--- public/mijnProject/index.php
+++ public/mijnProject/index.php
@@ -12,31 +12,44 @@ die("Verbinding mislukt: " . mysqli_connect_error());
 }
 else {
 // Dit gedeelte laat je normaliter weg, maar is hier ter illustratie toegevoegd
-echo '<i>verbinding database succesvol</i>';
+echo '<i>verbinding database succesvol</i>'.isset($_SESSION["user"]);
 }
 
 if(isset($_POST['submit'])) {
     $naam=$_POST['gebruikersnaam'];
     $pass=$_POST['wachtwoord'];
-    $sql="SELECT * FROM users WHERE (username='".$naam."' OR email='".$naam."')AND password='".$pass."' LIMIT 1";
+    $sql="SELECT * FROM users WHERE (username='".$naam."' OR email='".$naam."') LIMIT 1";
     $records = mysqli_query($DBverbinding, $sql);
+    $row = mysqli_fetch_array($records);
+    $passwordhash = $row["passwordhash"];
+    $username = $row["username"];    
     if (mysqli_num_rows($records) == 1){
-        $_SESSION["user"]= mysqli_fetch_array($records)["username"];   
-        //UPDATE IP bij user, pakt alleen ip van gitpod, werkt wel lokaal
-        $query = "UPDATE users SET lastIP='$_SERVER[REMOTE_ADDR]' WHERE username='$_SESSION[user]'";
-        mysqli_query($DBverbinding, $query);
-        header("Location: index.php");
-        exit();
+        if(password_verify($pass,$passwordhash)){
+            $_SESSION["user"]= $username;   
+            //UPDATE IP bij user, pakt alleen ip van gitpod, werkt wel lokaal
+            $query = "UPDATE users SET lastIP='$_SERVER[REMOTE_ADDR]' WHERE username='$username'";
+            mysqli_query($DBverbinding, $query);
+            header("Location: index.php");
+            exit();
+        }
+        else {
+            echo "<h1 style='color: red;'>Gebruikersnaam, E-mail of wachtwoord onjuist!</h1>";
+        }
+
     }
     else {
-        echo "<h1 style='color: red;'>Gebruikersnaam of wachtwoord onjuist</h1>";
+        echo "<h1 style='color: red;'>Gebruikersnaam, E-mail of wachtwoord onjuist!</h1>";
     }
 }
+
 if(isset($_POST['loguit'])) {
     
-    echo "<script type='text/javascript'>alert('uitgelogd!');</script>";
     session_destroy();
     header("Location: index.php");
+    echo "<h1 style='color: red;'>uitgelogd!</h1>";
+    echo "<script type='text/javascript'>alert('uitgelogd!');</script>";
+
+
 }
 
 if(isset($_POST['signup'])) {
@@ -56,6 +69,9 @@ else {
     <head>
         <title>Startpagina</title>
         <link rel="stylesheet" type="text/css" href="css/design.css">
+        <!-- hcaptcha -->
+        <script src="https://hcaptcha.com/1/api.js" async defer></script>
+
     </head>
     <body>
         <div id="container">
@@ -70,6 +86,9 @@ else {
                 <label>Wachtwoord</label>
                 <input type="password" name="wachtwoord" placeholder="Geef uw wachtwoord...">
                 <input type="submit" name="submit"><br><br>
+
+                <!-- hcaptcha -->
+                <div class="h-captcha" data-sitekey="254a11ac-8587-4306-aa5b-52e6d9f2d227"></div>
 
                 <input type="submit" name="signup" value="Geen account? Klik hier!">
             </form>';
--- public/mijnProject/signup.php
+++ public/mijnProject/signup.php
@@ -17,14 +17,14 @@ echo '<i>verbinding database succesvol</i>';
 
 if(isset($_POST['submit'])) {
     $naam=$_POST['gebruikersnaam'];
-    $pass=$_POST['wachtwoord'];
+    $pass=password_hash($_POST['wachtwoord'], PASSWORD_BCRYPT);
     $email=$_POST['email'];
     $sql="SELECT * FROM users WHERE (username='".$naam."' OR email='".$email."')LIMIT 1";
     $records = mysqli_query($DBverbinding, $sql);
     if (mysqli_num_rows($records) == 0){
        
 
-        $query = "INSERT INTO users(username, password, email) VALUES ('$naam', '$pass', '$email')";
+        $query = "INSERT INTO users(username, passwordhash, email, enabled) VALUES ('".$naam."', '".$pass."', '".$email."', 1)";
         mysqli_query($DBverbinding, $query);
         header("Location: signup.php");
         exit();
@@ -40,6 +40,9 @@ if(isset($_POST['submit'])) {
     <head>
         <title>Startpagina</title>
         <link rel="stylesheet" type="text/css" href="css/design.css">
+         <!-- hcaptcha -->
+        <script src="https://hcaptcha.com/1/api.js" async defer></script>
+
     </head>
     <body>
         <div id="container">
@@ -47,17 +50,22 @@ if(isset($_POST['submit'])) {
                 <?php echo 'een <strong>klein</strong> stukje PHP<br>';?>
             </h1>
             <?php if(!$_SESSION['user']) { echo'
-            <form method="POST" action="">
-                <label>Gebruiker</label>
-                <input type="text" name="gebruikersnaam" placeholder="Kies een gebruikersnaam..."><br><br>
-                <label>Wachtwoord</label>
-                <input type="password" name="wachtwoord" placeholder="Geef uw wachtwoord..."><br><br>
-                <label>email</label>
-                <input type="text" name="email" placeholder="Geef uw email...">
-                <input type="submit" name="submit" value="registreer">
-            </form>';}
+                <form method="POST" action="">
+                    <label>Gebruiker</label>
+                    <input type="text" name="gebruikersnaam" placeholder="Kies een gebruikersnaam..."><br><br>
+                    <label>Wachtwoord</label>
+                    <input type="password" name="wachtwoord" placeholder="Geef uw wachtwoord..."><br><br>
+                    <label>email</label>
+                    <input type="text" name="email" placeholder="Geef uw email...">
+                    <input type="submit" name="submit" value="registreer"><br><br>
+
+                    <!-- hcaptcha -->
+                    <div class="h-captcha" data-sitekey="254a11ac-8587-4306-aa5b-52e6d9f2d227"></div>
+                
+                    </form>';
+            }
             else {
-                echo "<h1 style='color: red;'>U heeft een account en u bent ingelogd.</h1>";
+                echo "<h1 style='color: red;'>U heeft al een account en u bent al ingelogd.</h1>";
             }?>
         </div>
     </body>
